8617a3da5495f0c907642df588459dfe4c2bc445
Pouya Kary <k@karyfoundation.org>
2016-09-15 03:34:43 +0430

    min

--- build.sh
+++ build.sh
@@ -5,20 +5,14 @@
 
     gulp
 
-    #cd ./_compiled
-    #electron main.js
-    #cd ..
-
-#
-# ─── ASAR PART ──────────────────────────────────────────────────────────────────
-#
-
-    asar pack _compiled orchestraCore.asar
+    cd ./_compiled
+    electron main.js
+    cd ..
 
 #
 # ─── PACKING ────────────────────────────────────────────────────────────────────
 #
 
-	electron-packager ./_compiled "Orchestra" --platform=darwin --arch=x64 --app-copyrigh="Copyright 2016 by Kary Foundation, Inc." --app-version="1" --icon=designs/icon/icns/icon.icns --name="Orchestra" --out=release --overwrite=true
+    #	electron-packager ./_compiled "Orchestra" --platform=darwin --arch=x64 --app-copyrigh="Copyright 2016 by Kary Foundation, Inc." --app-version="1" --icon=designs/icon/icns/icon.icns --name="Orchestra" --out=release --overwrite=true
 
 	#electron-packager ./binary "Comment IV" --platform=win32 --arch=x64 --app-copyrigh="Copyright 2016 by Kary Foundation, Inc." --app-version="IV.3.124" --icon=icon/ico/icon.ico --name="Comment IV" --out=release --overwrite=true
