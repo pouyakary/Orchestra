bde6483e7c579def95c650f37d3dae52079ffc62
Pouya Kary <k@karyfoundation.org>
2016-09-15 03:19:28 +0430

    electron you beast

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
-gulp
 
-cd ./_compiled
-electron main.js
-cd ..
+#
+# ─── GULP BUILD ─────────────────────────────────────────────────────────────────
+#
+
+    gulp
+
+    #cd ./_compiled
+    #electron main.js
+    #cd ..
+
+#
+# ─── ASAR PART ──────────────────────────────────────────────────────────────────
+#
+
+    asar pack _compiled orchestraCore.asar
+
+#
+# ─── PACKING ────────────────────────────────────────────────────────────────────
+#
+
+	electron-packager ./_compiled "Orchestra" --platform=darwin --arch=x64 --app-copyrigh="Copyright 2016 by Kary Foundation, Inc." --app-version="1" --icon=designs/icon/icns/icon.icns --name="Orchestra" --out=release --overwrite=true
+
+	#electron-packager ./binary "Comment IV" --platform=win32 --arch=x64 --app-copyrigh="Copyright 2016 by Kary Foundation, Inc." --app-version="IV.3.124" --icon=icon/ico/icon.ico --name="Comment IV" --out=release --overwrite=true
