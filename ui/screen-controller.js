5f567098fe13b50b0687a75431cb4fea63a3e74f
Pouya Kary <k@karyfoundation.org>
2016-09-15 23:01:56 +0430

    tab controllers added

--- ui/screen-controller.js
+++ ui/screen-controller.js
@@ -3,3 +3,37 @@
 // Copyright 2016 Kary Foundation, Inc.
 //   Author: Pouya Kary <k@karyfoundation.org>
 //
+
+//
+// ─── DEFS ───────────────────────────────────────────────────────────────────────
+//
+
+    const EditorWindowScreenElement = 'EditorView';
+    const PlaygroundWindowScreenElement = 'PlaygroundView';
+    const TabsPlaceholder = 'header-tabs';
+
+    var CurrentActiveView = 'editor';
+
+//
+// ─── CHANGE TO EDITOR ───────────────────────────────────────────────────────────
+//
+
+    function onChangeWindowToEditor ( ) {
+        CurrentActiveView = 'editor';
+        document.getElementById( EditorWindowScreenElement ).hidden = false;
+        document.getElementById( PlaygroundWindowScreenElement ).hidden = true;
+        document.getElementById( TabsPlaceholder ).className = 'editor-tab';
+    }
+
+//
+// ─── CHANGE TO PLAYGROUND ───────────────────────────────────────────────────────
+//
+
+    function onChangeWindowToPlayground ( ) {
+        CurrentActiveView = 'playground';
+        document.getElementById( EditorWindowScreenElement ).hidden = true;
+        document.getElementById( PlaygroundWindowScreenElement ).hidden = false;
+        document.getElementById( TabsPlaceholder ).className = 'playground-tab';
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
