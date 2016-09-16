bd77aa202739d6870c219a9caec636965730e5ac
Pouya Kary <k@karyfoundation.org>
2016-09-17 00:09:45 +0430

    IPC and Remote tools have added. So system now works right

---
+++ ui/window.js
@@ -0,0 +1,22 @@
+
+//
+// Copyright 2016 Kary Foundation, Inc.
+//   Author: Pouya Kary <k@karyfoundation.org>
+//
+
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
+
+    const { ipcRenderer } = require( 'electron' );
+    const OrchestraWindow = require( 'electron' ).remote.getCurrentWindow( );
+
+//
+// ─── MESSAGES ───────────────────────────────────────────────────────────────────
+//
+
+    function fireWindowCloseRequest ( ) {
+        ipcRenderer.send( 'window-status', 'close' );
+    }
+
+// ────────────────────────────────────────────────────────────────────────────────
