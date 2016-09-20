ad040d459edd6b6d165ae7cc8a6ab586fbe278f9
Pouya Kary <k@karyfoundation.org>
2016-09-21 03:08:00 +0330

    Sheets style is not good for unmaximixed views

--- core/ui/dialogs.js
+++ core/ui/dialogs.js
@@ -46,13 +46,19 @@
 
     function checkPathAndAskForPathIfNeeded ( forceAsk = false ) {
         if ( currentFile.path === defaultEmptyPath || forceAsk ) {
-            let newPath = dialog.showSaveDialog( OrchestraWindow, {
-                title: "Choose a path for your file",
-                filters: [{
-                    name: 'Quartet',
-                    extensions: [ 'quartet' ],
-                }]
-            });
+            const newPath = dialog.showSaveDialog(
+                // use sheet style for full screen and separate window for normal:
+                ( OrchestraWindow.isMaximized( ) )? OrchestraWindow : null,
+                // options
+                {
+                    title: "Choose a path for your file",
+                    filters: [{
+                        name: 'Quartet',
+                        extensions: [ 'quartet' ],
+                    }]
+                }
+            );
+
             if ( newPath !== undefined ) {
                 currentFile.path = newPath;
                 return true;
