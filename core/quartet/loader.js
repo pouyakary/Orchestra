c65f295fe9fad296c1a03eacd44f66f260b9d8fb
Pouya Kary <k@karyfoundation.org>
2016-10-01 12:27:33 +0330

    LICENSING...

--- core/quartet/loader.js
+++ core/quartet/loader.js
@@ -3,6 +3,10 @@
 // Copyright 2016 Kary Foundation, Inc.
 //   Author: Pouya Kary <k@karyfoundation.org>
 //
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 
 //
 // ─── LOAD NEW WORKSPACE ─────────────────────────────────────────────────────────
