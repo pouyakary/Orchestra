0c5efc91d700148cfa6936742a5bac99e17eb75c
Pouya Kary <k@karyfoundation.org>
2016-11-07 00:35:02 +0330

    min...

--- editor/quartet/base.js
+++ editor/quartet/base.js
@@ -30,7 +30,6 @@
             scrollbars: true,
             // trashcan: true,
             media: './blockly-core/media/',
-            // css: false,
         })
 
         Blockly.Xml.domToWorkspace( Blockly.Xml.textToDom( defaultFileXML ), workspace )
